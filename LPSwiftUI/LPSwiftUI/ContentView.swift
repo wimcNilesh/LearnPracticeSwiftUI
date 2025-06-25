afd313f731052def6f1c50f45fa3968fb9192e6a
Nilesh Kumar <techynilesh7@gmail.com>
2025-06-25 07:12:21 +0530

    Setting xcode project

---
+++ LPSwiftUI/LPSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LPSwiftUI
+//
+//  Created by Nilesh.61045877 on 25/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LPSwiftUI/LPSwiftUI/LPSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LPSwiftUIApp.swift
+//  LPSwiftUI
+//
+//  Created by Nilesh.61045877 on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LPSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
